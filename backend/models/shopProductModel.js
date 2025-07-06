c930d66bb32df602698896685ff17b5f027ce7f2
Himasha Chinthani <140165425+HimashaChinthani@users.noreply.github.com>
2025-07-06 15:37:20 +0530

    Create  shopProductModel.js

---
+++ backend/models/shopProductModel.js
@@ -0,0 +1,36 @@
+const {pool} =require('../config/database');
+const ShopProductModel= {
+ creae:async(product)=>{
+    const[result]=await pool.execute(
+        `INSERT INTO shop_products (
+        shop_name, owner_name, email, phone_no, shop_address, city,
+        product_name, brand, category, season, price, unit,
+        available_quantity, product_description, usage_history
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        product.shop_name,
+        product.owner_name,
+        product.email,
+        product.phone_no,
+        product.shop_address,
+        product.city,
+        product.product_name,
+        product.brand,
+        product.category,
+        product.season,
+        product.price,
+        product.unit,
+        product.available_quantity,
+        product.product_description,
+        product.usage_history
+      ]  
+    );
+    return result;
+ },
+ getAll: async () => {
+    const [rows] = await pool.execute('SELECT * FROM shop_products');
+    return rows;
+  }
+};
+
+module.exports=ShopProductModel;
