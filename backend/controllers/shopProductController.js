14aed9d61789c5dd676997708b8cd913e21cc4ed
Himasha Chinthani <140165425+HimashaChinthani@users.noreply.github.com>
2025-07-06 15:42:48 +0530

    create  shopProductController.js

---
+++ backend/controllers/shopProductController.js
@@ -0,0 +1,47 @@
+const ShopProductModel=require('../models/shopProductModel')
+
+exports.createShopproduct=async(requestAnimationFrame,res)=>{
+try{
+   const {
+      shop_name,
+      owner_name,
+      email,
+      phone_no,
+      shop_address,
+      city,
+      product_name,
+      brand,
+      category,
+      season,
+      price,
+      unit,
+      available_quantity,
+      product_description,
+      usage_history
+    } = req.body; 
+
+    if(!shop_name||!owner_name||!email|| !product_name || !price) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    const result = await ShopProductModel.create(req.body);
+    res.status(201).json({
+      message: ' Shop product added successfully',
+      shopitemid: result.insertId
+    });
+    } catch (error) {
+    console.error(' Error creating shop product:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+exports.getAllShopProducts = async (req, res) => {
+  try {
+    const products = await ShopProductModel.getAll();
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(' Error fetching shop products:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
