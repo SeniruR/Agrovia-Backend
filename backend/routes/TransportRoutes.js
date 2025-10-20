6dddb67045e679ec696531a7e6b719e7681aa0f8
Himasha Chinthani <140165425+HimashaChinthani@users.noreply.github.com>
2025-10-20 19:44:30 +0530

    update TransportRoutes.js

--- backend/routes/TransportRoutes.js
+++ backend/routes/TransportRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const transportController = require('../controllers/transportAllocationController');
+const transportController = require('../controllers/TransportAllocationController');
 
 // Create a new transport allocation
 router.post('/', transportController.createTransportAllocation);
